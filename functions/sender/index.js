778dc2f472dd9b1f785dcacd80e8e86c22dfa57a
ahmedajx <ahassan3009@gmail.com>
2017-02-28 21:57:59 +0000

    tabs to spaces

--- functions/sender/index.js
+++ functions/sender/index.js
@@ -9,20 +9,20 @@ var client = new twilio.RestClient(
 );
 
 exports.handle = function() {
-	http.get("http://skysportsapi.herokuapp.com/sky/football/getteamnews/chelsea/v1.0/", function (response) {  
-		response.setEncoding('utf8');
-		var news = '';
-		response.on('data', function(data){
-		  	news += data;
-		});
-		response.on('end',function(){
-		    client.messages.create({
-			    body: JSON.parse(news)[0].shortdesc,
-			    to: process.env.FROMTEL,
-			    from: process.env.TOTEL
-			}, function(err, message) {
-			    console.log(message.sid);
-			});
-		});
-	})
+    http.get("http://skysportsapi.herokuapp.com/sky/football/getteamnews/chelsea/v1.0/", function (response) {
+        response.setEncoding('utf8');
+        var news = '';
+        response.on('data', function(data){
+            news += data;
+        });
+        response.on('end',function(){
+            client.messages.create({
+                body: JSON.parse(news)[0].shortdesc,
+                to: process.env.FROMTEL,
+                from: process.env.TOTEL
+            }, function(err, message) {
+                console.log(message.sid);
+            });
+        });
+    })
 }
